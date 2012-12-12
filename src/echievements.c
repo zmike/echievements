38e3d8209e677062e4d78da53ff858e3f0b4192f
discomfitor <michael.blumenkrantz@gmail.com>
2012-12-12 21:21:47 +0000

    add trophies to gamescore so they get saved

--- src/echievements.c
+++ src/echievements.c
@@ -49,6 +49,7 @@ _ech_add(Echievement_Id id)
 
    et = etrophy_trophy_new(Echievement_Strings[id], Echievement_Descs[id],
                            Echievement_Hide_States[id], Echievement_Goals[id]);
+   etrophy_gamescore_trophy_add(ech_config->gs, et);
    _ech_list_add(et);
 }
 
